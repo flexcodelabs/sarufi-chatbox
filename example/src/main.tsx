51b3af88b323127473b3892793dc4aaa16b5cf65
Ben <bmsteve96@gmail.com>
2023-03-16 00:13:12 +0300

    fix plugin cdn scripts to accept botId from window object to fetch plugin coonfigurations (release)

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SarufiChatbox, { MediaPreview } from "../../src/";
+// @ts-ignore
+window.botId = 71
 
 ReactDOM.createRoot(
   document.getElementById("sarufi-chatbox") as HTMLElement
 ).render(
   <React.StrictMode>
-    <div>
-      <h1>My Beautiful Website</h1>
-    </div>
-    <SarufiChatbox botId={16} />
+    {/* @ts-ignore */}
+    <SarufiChatbox botId={window?.botId ?? 16} />
   </React.StrictMode>
 );

05295e6a22e7eaddcd3a7df96e3407e30bb243b8
Elia <mkumboelia@gmail.com>
2024-07-31 14:32:50 +0300

    release: 🏗️ build: created build scripts and styles for deployment
    refactor: updated types location

--- src/chat/chat.tsx
+++ src/chat/chat.tsx
@@ -8,6 +8,29 @@ import { Close, MinusIcon } from "../assets/icons";
 import { SarufiIcon } from "../assets/illustrations";
 import { getChatId } from "./services/chat-id-handler";
 
+interface ThemeType {
+  buttonSize?: "sm" | "md" | "lg";
+  primaryColor?: string;
+  fontSize?: string | number;
+  fontFamily?: "PoppinsRegular" | "InterRegular" | "inherit";
+  borderColor?: string;
+  sentBoxBg?: string;
+  receivedBoxBg?: string;
+  sentBoxColor?: string;
+  receivedBoxColor?: string;
+  chatboxBg?: string;
+  receivedBoxLinkColor?: string;
+  sentBoxLinkColor?: string;
+  mode?: "light" | "dark";
+  title?: string;
+  placement?: "left" | "right";
+  height?: string | number;
+  width?: string | number;
+  secondaryColor?: string;
+  bubbleTitle?: string;
+  bubbleSubTitle?: string;
+}
+
 export type PluginDBData = {
   id: string | number;
   bot_id: string;
